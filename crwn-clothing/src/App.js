a6b0d5e0cb40b2226e91ea425423dcdfddd37f07
Louie Williford <Louie@passwords-MacBook-Pro.local>
2019-10-20 11:19:02 -0600

    app init, built basic store components and auth components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/sign-in" component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
