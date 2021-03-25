5611df2e63900ab1ec58fee3f60166f41eb5a1ab
lou <dustyfingers01257@gmail.com>
2021-03-25 11:04:05 -0600

    repo restructure

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -9,16 +9,16 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/sign-in" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+        <Header />
+        <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/sign-in" component={SignInAndSignUpPage} />
+        </Switch>
+        </div>
+    );
 }
 
 export default App;
