4fe2aef6540295ce7a7a821403ea7cb562711ad1
LouieW <dustyfingers01257@gmail.com>
2022-01-07 22:35:25 -0700

    added ability to add multiple items to cart, updated shop data

--- crwn-clothing/src/redux/cart/cart.reducer.js
+++ crwn-clothing/src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils'; 
 
 const INITIAL_STATE = {
     hidden: true,
@@ -13,9 +14,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 hidden: !state.hidden
             };
         case CartActionTypes.ADD_ITEM:
+            console.log(state)
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             };
         default:
             return state;
